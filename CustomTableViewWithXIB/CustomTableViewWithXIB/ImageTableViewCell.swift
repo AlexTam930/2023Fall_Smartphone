a27e65fdc6aeadd1dc4b554dc192b95e0ae51bd2
AlexTam <alextam930@outlook.com>
2023-10-02 16:29:39 -0700

    1. Create an app for Custom table view With XIB file to show different food items.

---
+++ CustomTableViewWithXIB/CustomTableViewWithXIB/ImageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ImageTableViewCell.swift
+//  CustomTableViewWithXIB
+//
+//  Created by Alex on 2023/10/2.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

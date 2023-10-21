16efd893028bcaa73eb6501f6a5129e2e96fb6c1
AlexTam <alextam930@outlook.com>
2023-10-21 16:59:01 -0700

    second update.

---
+++ StockApp/StockApp/AddStockViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AddStockViewController.swift
+//  StockApp
+//
+//  Created by Alex on 2023/10/21.
+//
+
+import UIKit
+
+class AddStockViewController: UITableController {
+
+}
---
+++ StockApp/StockApp/Utils/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  StockApp
+//
+//  Created by Alex on 2023/10/21.
+//
+
+import Foundation
